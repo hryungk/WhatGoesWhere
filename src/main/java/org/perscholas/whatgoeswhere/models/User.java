d921764eede7190ecead1af4861a1db43129d60c
HoppyCat <hryungk@gmail.com>
2021-07-18 19:03:57 -0700

    Created models

---
+++ src/main/java/org/perscholas/whatgoeswhere/models/User.java
@@ -0,0 +1,109 @@
+package org.perscholas.whatgoeswhere.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Users")
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User {
+
+	@Id
+	@Column(name="id", length=50)
+	private String id; // User ID
+	@Column(name="password", length=50, nullable=false)
+	private String password; // User password
+	@Column(name="name", length=50)
+	private String name; // User's full name
+	@Column(name="email", length=50) 
+	private String email;
+	@OneToMany(targetEntity = Item.class)
+	@JoinTable(
+			name = "User_Item",
+			joinColumns = 
+				{ @JoinColumn(name = "user_id", referencedColumnName = "id")}, 
+			inverseJoinColumns = 
+				{ @JoinColumn(name = "item_id", referencedColumnName = "id")})
+	private List<Item> items; // A list of items the user has added to the system
+	
+	public User() {
+		super();
+		id = "";		
+		password = "";		
+		email = "";
+		name = "";
+		items = new ArrayList<>();
+	}
+	public User(String id, String password, String email, String name, List<Item> items) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.email = email;
+		this.name = name;		
+		this.items = items;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Item> getItems() {
+		return items;
+	}
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+	
+}
---
+++ src/main/java/org/perscholas/whatgoeswhere/models/UserItem.java
@@ -0,0 +1,81 @@
+/*
+ * Filename: UserItem.java
+* Author: Helen Kim
+* 07/18/2021 
+ */
+package org.perscholas.whatgoeswhere.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ * @author Harry
+ *
+ */
+@Entity // This should be commented out when first creating tables in the database.
+@Table(name="User_Item")
+@IdClass(UserItemID.class)
+@NamedQuery(name="ItemsByUser", query="SELECT ui FROM UserItem ui WHERE ui.userId = :id")
+public class UserItem {
+	@Id
+	@Column(name="user_id")
+	private String userId;
+
+	@Id
+	@Column(name="item_id")
+	private int itemId;
+
+	public UserItem() {
+		userId = "";
+		itemId = 0;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public int getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + itemId;
+		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserItem other = (UserItem) obj;
+		if (itemId != other.itemId)
+			return false;
+		if (userId == null) {
+			if (other.userId != null)
+				return false;
+		} else if (!userId.equals(other.userId))
+			return false;
+		return true;
+	}	
+
+}
